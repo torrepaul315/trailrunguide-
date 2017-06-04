01aed05c2681740e8b672843a3fe8b6a0388cab4
Paul Torres <torrepaul@gmail.com>
2017-06-04 14:38:26 -0600

    figuring out hitting the backend

--- app/routes/best-websites.js
+++ app/routes/best-websites.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return['iRunFar','Trail Runner Magazine','Trail Sisters'];
+    return ['iRunFar','Trail Runner Magazine','Trail Sisters'];
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model() {
-  //   return this.get('store').findAll('goat');
-  // }
   model() {
-    return {
-      data: [{
-        type : 'kilian J info',
-        firstName: 'Kilian',
-        lastName: 'Jornet Burgada',
-        status: 'G.O.A.T',
-        image: ''
-      }]
-    }
-  }
+    return this.get('store').findAll('/');
+   }
+// I had this ('store')
+
+  // model() {
+  //   return {
+  //     data: {
+  //       type : 'kilian J info',
+  //       firstName: 'Kilian',
+  //       lastName: 'Jornet Burgada',
+  //       status: 'G.O.A.T',
+  //       image: ''
+  //     },
+  //   }
+  // }
 
 });
