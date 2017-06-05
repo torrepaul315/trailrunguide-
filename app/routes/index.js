bc79d454b14ef2ad10e6275110e2429da261cf93
Paul Torres <torrepaul@gmail.com>
2017-06-05 10:23:21 -0600

    blew out mirage nonsense

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('/goat');
+    return this.get('store').findAll('goat');
    }
 // I had this ('store')
 
