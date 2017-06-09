4f5c1bb0a8b31d36b386f3393a05b4204e20ea54
Paul Torres <torrepaul@gmail.com>
2017-06-09 13:35:34 -0600

    added in stava widget

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
   // host: "http://localhost:3000"
 
-  //my heroku backend! woot woot! 
+  //my heroku backend! woot woot!
   host: "https://trguidebackend.herokuapp.com"
 });
