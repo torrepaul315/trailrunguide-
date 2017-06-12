3a6d15bc9e4b62989edbd268a7b225bed0b57c24
Paul Torres <torrepaul@gmail.com>
2017-06-11 19:52:39 -0600

    took out funky scripts that screwed up server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  // host: "http://localhost:3000"
+  host: "http://localhost:3000"
 
   //my heroku backend! woot woot!
-  host: "https://trguidebackend.herokuapp.com"
+  // host: "https://trguidebackend.herokuapp.com"
 });
